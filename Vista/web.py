cd6c357d46d646866e9d1cf831c1c4d6c0ab6f48
Tomas <tomasperesin@gmail.com>
2023-02-27 19:03:43 -0300

    Busqueda el ML + pagina en flask

---
+++ Vista/web.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for
+from Modelo import Busqueda
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('busqueda'))
+
+
+@app.route('/busqueda', methods=['GET', 'POST'])
+def busqueda():
+    if request.method == 'POST':
+        producto = request.form['producto']
+        tabla = Busqueda.busqueda_mercado_libre(producto)
+        return render_template('muestra.html', lista_producto=tabla)
+    else:
+        return render_template('busqueda.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
